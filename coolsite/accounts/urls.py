382eda3660978473ec2368fbbc4aea05f965b3b9
Computer <tavisevik@yandex.ru>
2023-09-30 01:55:43 +0300

    First

---
+++ coolsite/accounts/urls.py
@@ -0,0 +1,12 @@
+from accounts.views import *
+from django.urls import path
+
+
+urlpatterns = [
+    path('', index, name='account'),    #http://127.0.0.1:8000/
+    path('view/', viewData(), name='view'),
+    path('registration/', RegistrationUser.as_view(), name='registration'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('update/<int:user_id>/', RegistrationUser.as_view(), name='update_user'),
+    path('delete/<int:user_id>/', RegistrationUser.as_view(), name='delete_user'),
+]
